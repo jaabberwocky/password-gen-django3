18da9e39f3031d35190d2b0e496eb23134fc0202
tobias <tobias@dsaid.gov.sg>
2021-02-15 12:39:14 +0800

    Define views

--- password_gen/password_generator-project/generator/views.py
+++ password_gen/password_generator-project/generator/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return HttpResponse("<h1>Hello there friend!</h1>")
+
+def eggs(request):
+    return HttpResponse("<h3>You got eggs?</h3>")
