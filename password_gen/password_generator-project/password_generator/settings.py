c3b3c762c1d0ad0f2197fb440c1f57dcff58eed3
tobias <tobias@dsaid.gov.sg>
2021-02-16 13:14:41 +0800

    Add gunicorn and disable debug

--- password_gen/password_generator-project/password_generator/settings.py
+++ password_gen/password_generator-project/password_generator/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'Optional default value')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if os.getenv('PROD', True):
+    DEBUG = False
 
 ALLOWED_HOSTS = []
 
